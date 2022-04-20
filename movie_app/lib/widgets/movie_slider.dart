dd8195612b91cd2547d1d496ee3334c305c2bd73
WardedC <edw23cabrera@gmail.com>
2022-04-20 12:49:26 -0500

    Update MovieSlider

--- movie_app/lib/widgets/movie_slider.dart
+++ movie_app/lib/widgets/movie_slider.dart
@@ -7,15 +7,17 @@ class MovieSlider extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       width: double.infinity,
-      height: 250,
-      color: Colors.red,
+      height: 272,
+      //color: Colors.red,
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           const Padding(
-            padding: EdgeInsets.all(8.0),
+            padding: EdgeInsets.symmetric(horizontal: 20),
             child: Text('Populars', style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),),
           ),
+          
+          const SizedBox( height: 5,),
           Expanded(
             child: ListView.builder(
               itemCount: 20,
@@ -39,8 +41,30 @@ class _MoviePoster extends StatelessWidget {
     return Container(
       width: 130,
       height: 190,
-      color: Colors.green,
-      margin: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+      //color: Colors.green,
+      margin: EdgeInsets.symmetric(horizontal: 10),
+      child: Column(
+        children: [
+          GestureDetector(
+            onTap: () => Navigator.pushNamed(context, 'Details', arguments:  'Movie-instance'),
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(20),
+              child: FadeInImage(
+                placeholder: AssetImage('assets/no-image.jpg'), 
+                image: NetworkImage('https://via.placeholder.com/300x400'),
+                width: 130,
+                height: 190,
+                fit: BoxFit.cover,),
+            ),
+          ),
+          Text(
+            'StarWars: una guerra de locos',
+            maxLines: 4,
+            overflow: TextOverflow.ellipsis,
+            textAlign: TextAlign.center,
+            )
+        ],
+      ),
     );
   }
 }
