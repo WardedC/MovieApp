c393c9c3c522a7c6383db7435a79468dd7752038
WardedC <edw23cabrera@gmail.com>
2022-04-20 17:18:30 -0500

    DetailsScreen placehoder

--- movie_app/lib/screens/details_screen.dart
+++ movie_app/lib/screens/details_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:movie_app/widgets/widgets.dart';
 
 class DetailsScreen extends StatelessWidget {
   const DetailsScreen({Key? key}) : super(key: key);
@@ -10,8 +11,18 @@ class DetailsScreen extends StatelessWidget {
     return Scaffold(
       //appBar: AppBar(),
       body: CustomScrollView(
+        
         slivers: [
-          _CustomAppBar()
+          _CustomAppBar(),
+          SliverList(
+            delegate: SliverChildListDelegate([
+              _PosterAndTitle(),
+              _Overview(),
+              CastingCard(),
+              SizedBox(height: 10,)
+
+            ]) 
+          )
         ],
       )
     );
@@ -19,19 +30,22 @@ class DetailsScreen extends StatelessWidget {
 }
 
 class _CustomAppBar extends StatelessWidget {
+
   //const _CustomAppBar({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return SliverAppBar(
-      expandedHeight: 200,
+      expandedHeight: 250,
       backgroundColor: Colors.indigo,
       floating: false,
       pinned: true,
+      stretch: true,
       flexibleSpace: FlexibleSpaceBar(
         centerTitle: true,
         titlePadding: const EdgeInsets.all(0),
         title: Container(
+          padding: EdgeInsets.only(bottom: 20),
           color: Colors.black38,
           width: double.infinity,
           alignment: Alignment.bottomCenter,
@@ -50,6 +64,79 @@ class _CustomAppBar extends StatelessWidget {
       ),
       
       
+    );
+  }
+}
+
+class _PosterAndTitle extends StatelessWidget {
+
+  //const _PosterAndTitle({Key? key}) : super(key: key);
+ 
+
+  @override
+  Widget build(BuildContext context) {
+     final TextTheme textTheme = Theme.of(context).textTheme;
+    return Container(
+      margin: EdgeInsets.only(top: 20),
+      padding: EdgeInsets.symmetric(horizontal: 20),
+      child: Row(
+        children: [
+          ClipRRect(
+            borderRadius: BorderRadius.circular(20),
+            child: const FadeInImage(
+              height: 190,
+              width: 150,
+              placeholder: AssetImage('assets/loading.gif'),
+              image: NetworkImage('https://via.placeholder.com/400x300'),
+              fit: BoxFit.cover,
+            ),
+          ),
+          const SizedBox( width: 20,),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                'Movie.title',
+                style: textTheme.headline5,
+                overflow: TextOverflow.ellipsis,
+                maxLines: 2,
+              
+              ),
+              Text(
+                'Movie.title',
+                style: textTheme.subtitle1,
+                overflow: TextOverflow.ellipsis,
+              ),
+              Row(
+                children: [
+                  Icon(Icons.star, size: 20, color: Colors.amber,),
+                  SizedBox(width: 5,),
+                  Text(
+                    'movie.voteAverage',
+                    style: textTheme.caption,
+                    )
+                ],
+              )              
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class _Overview extends StatelessWidget {
+  //const _Overview({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 30, vertical: 10),
+      child: Text(
+        'Enim consectetur aute eu irure ipsum id sunt. Eiusmod est labore adipisicing esse ut laborum tempor ex commodo fugiat irure incididunt officia cupidatat. Mollit nostrud deserunt nostrud dolore laborum anim aute amet adipisicing nisi commodo sunt esse. Anim do veniam laboris aute qui tempor esse tempor ea velit veniam voluptate. Veniam quis do cillum qui ullamco voluptate culpa ipsum ullamco nostrud. Ullamco eu consequat proident ipsum occaecat eiusmod ea officia nostrud. Elit veniam nostrud ea culpa nulla Lorem. Id commodo laboris ut veniam dolore culpa fugiat sit enim qui. Et sint ea exercitation exercitation anim ipsum tempor mollit commodo cillum. Eu exercitation mollit consectetur sint. Ad consequat proident fugiat amet. Ullamco deserunt Lorem aute proident sit eu dolore.',
+        textAlign: TextAlign.justify,
+        style: Theme.of(context).textTheme.subtitle1,
+        ),
     );
   }
 }
---
+++ movie_app/lib/widgets/casting_cards.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+
+class CastingCard extends StatelessWidget {
+  //const CastingCard({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      //margin: EdgeInsets.only(bottom: 30),
+      width: double.infinity,
+      height: 180,
+      //color: Colors.red,
+      child: ListView.builder(
+        itemCount: 10,
+        scrollDirection: Axis.horizontal,
+        itemBuilder: ((context, index) {
+          return _CastCard();
+        })
+      ),
+    );
+  }
+}
+
+class _CastCard extends StatelessWidget {
+  const _CastCard({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.symmetric(horizontal: 10),
+      width: 110,
+      height: 100,
+      //color: Colors.green,
+      child: Column(
+        children: [
+          GestureDetector(
+            onTap: () => Navigator.pushNamed(context, 'Details', arguments:  'Movie-instance'),
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(20),
+              child: const FadeInImage(
+                placeholder: AssetImage('assets/no-image.jpg'), 
+                image: NetworkImage('https://via.placeholder.com/300x400'),
+                width: 140,
+                height: 140,
+                fit: BoxFit.cover,),
+            ),
+          ),
+          SizedBox(height: 5,),
+          const Text(
+            'Tomi Mawaier puentes puentes',
+            maxLines: 2,
+            overflow: TextOverflow.ellipsis,
+            textAlign: TextAlign.center,
+            )
+        ],
+      ),
+    );
+  }
+}
