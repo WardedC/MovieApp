a31b572baff9bcc0b781224550324ce5e9c5f49d
WardedC <edw23cabrera@gmail.com>
2022-04-30 15:37:59 -0500

    Search Movies Works!

--- movie_app/lib/search/search_delegate.dart
+++ movie_app/lib/search/search_delegate.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:movie_app/models/models.dart';
+import 'package:movie_app/provider/movies_provider.dart';
+import 'package:provider/provider.dart';
 
 class MovieSearchDelegate extends SearchDelegate {
 
@@ -40,16 +43,57 @@ class MovieSearchDelegate extends SearchDelegate {
   Widget buildSuggestions(BuildContext context) {
 
     if(query.isEmpty){
-      return Container(
-        child: Center(
+      return _isEntyContainer();
+    }
+
+    final moviesProvider = Provider.of<MovieProvider>(context, listen: false);
+    return FutureBuilder(
+      future: moviesProvider.searchMovies(query),
+      builder: (_, AsyncSnapshot<List<Movie>> snapshot){
+        if( !snapshot.hasData ) return _isEntyContainer();
+        final movies = snapshot.data;
+        return ListView.builder(
+          itemCount: movies!.length,
+          itemBuilder: (_, int index){
+            return _MovieItem(movie: movies[index]);
+          }
+
+        );
+      }
+    ); 
+  }
+
+  Widget _isEntyContainer(){
+    return Container(
+        child: const Center(
           child: Icon(Icons.movie_creation_outlined, color: Colors.redAccent, size: 100,),
         ),
       );
-    }
-
-    return Container();
   }
-
   
   
 }
+
+  class _MovieItem extends StatelessWidget {
+    final Movie movie;
+
+    const _MovieItem({Key? key, required this.movie}) : super(key: key);
+    
+  
+    @override
+    Widget build(BuildContext context) {
+      return ListTile(
+        leading: FadeInImage(
+          placeholder: AssetImage('assets/loading.gif'),
+          image: NetworkImage(movie.fullPosterImg),
+          width: 50,
+          fit: BoxFit.contain,
+        ),
+        title: Text(movie.title),
+        subtitle: Text(movie.originalTitle),
+        onTap: (){
+          Navigator.pushNamed(context, "Details", arguments: movie );
+        },
+      );
+    }
+  }
